0cc9704b4aedbffffb6fe71fe8c562b57c831ef3
Vasily Selivanov <gosh@evex.io>
2023-09-21 10:00:36 +0300

    added test for get_storage_proof

--- src/eth/mod.rs
+++ src/eth/mod.rs
@@ -13,6 +13,7 @@ pub mod helper;
 pub mod proof;
 mod transfer;
 
+#[derive(Debug)]
 pub struct StorageProofValue {
     pub value: U256,
     // Array of rlp-serialized MerkleTree-Nodes, starting with the storageHash-Node,
@@ -143,8 +144,8 @@ async fn get_storage_proof(
     block_num: Option<BlockNumber>
 ) -> anyhow::Result<Vec<StorageProof>> {
     let keys = vec![
-        U256::from_str_radix(&env::var("ELOCK_INDEX_TOTAL_SUPPLY")?, 10)?, // totalSupply storage slot
-        U256::from_str_radix(&env::var("ELOCK_INDEX_TRX_COUNT")?, 10)?, // trxCount storage slot
+        U256::from_str_radix(&env::var("ELOCK_INDEX_TOTAL_SUPPLY")?, 16)?, // totalSupply storage slot
+        U256::from_str_radix(&env::var("ELOCK_INDEX_TRX_COUNT")?, 16)?, // trxCount storage slot
     ];
 
     let proof = web3s
@@ -173,12 +174,13 @@ async fn get_storage_proof(
     Ok(storage_proofs)
 }
 
-
-mod test {
+#[cfg(test)]
+mod tests {
     use super::proof::serialize_block;
-    use super::{read_block, get_storage_proof};
+    use super::{StorageProof, read_block, get_storage_proof};
     use crate::helper::tracing::init_default_tracing;
     use std::env;
+    use std::matches;
     use std::str::FromStr;
     use web3::transports::WebSocket;
     use web3::types::{Address, BlockId, BlockNumber, U64};
@@ -195,6 +197,24 @@ mod test {
         let web3s = Web3::new(websocket);
         let block = read_block(&web3s, block_id).await?;
         serialize_block(block)?;
+        Ok(())
+    }
+
+    #[tokio::test]
+    pub async fn ensure_got_storage_proof() -> anyhow::Result<()> {
+        dotenv::dotenv().ok();
+        init_default_tracing();
+        let websocket = WebSocket::new(&env::var("ETH_NETWORK")?).await?;
+        let web3s = Web3::new(websocket);
+
+        let block_num = Some(BlockNumber::Latest);
+        let account = Address::from_str("0x52410a00621a9bc08f8230a27267957913d961b3")?;
+        let storage_proof = get_storage_proof(&web3s, account, block_num).await?;
+
+        assert_eq!(storage_proof.len(), 2);
+        assert!(matches!(storage_proof[0], StorageProof::TotalSupply { .. }));
+        assert!(matches!(storage_proof[1], StorageProof::TrxCount { .. }));
+
         Ok(())
     }
 }
