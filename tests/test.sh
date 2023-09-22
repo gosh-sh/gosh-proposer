f6b39a5941444aedae844469ba8d595a7a8ade2c
Alexander <alexander.s@gosh.sh>
2023-09-22 17:20:15 +0300

    set prop code in script

--- tests/test.sh
+++ tests/test.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -e
 set -o pipefail
+set -x
 
 gosh-cli config --is_json true -e https://sh.network.gosh.sh
 
-ADDRESS=$(gosh-cli -j genaddr --save --abi solidity/checker.abi.json --setkey keys.json solidity/checker.tvc | jq .raw_address | cut -d '"' -f 2)
+ADDRESS=$(gosh-cli -j genaddr --save --abi ../contracts/l2/checker.abi.json --genkey keys.json ../contracts/l2/checker.tvc | jq .raw_address | cut -d '"' -f 2)
 echo "address=$ADDRESS"
 gosh-cli -j callx --addr -1:9999999999999999999999999999999999999999999999999999999999999999 --abi SetcodeMultisigWallet.abi.json --keys devgiver9.json -m submitTransaction --value 10000000000 --bounce false --allBalance false --payload ""  --dest $ADDRESS
-gosh-cli -j deployx --abi solidity/checker.abi.json --keys keys.json solidity/checker.tvc
+gosh-cli -j deployx --abi ../contracts/l2/checker.abi.json --keys keys.json ../contracts/l2/checker.tvc
+PROP_CODE=$(gosh-cli -j decode stateinit --tvc ../contracts/l2/proposal.tvc | jq .code | cut -d '"' -f 2)
+gosh-cli -j callx --abi ../contracts/l2/checker.abi.json --keys keys.json --addr $ADDRESS -m setProposalCode --code $PROP_CODE
