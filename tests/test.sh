3db61535d272aece127cd1ea3788e64ad8385e9d
Alexander <alexander.s@gosh.sh>
2023-09-23 13:48:12 +0300

    fix

--- tests/test.sh
+++ tests/test.sh
@@ -17,6 +17,6 @@ echo "address=$ADDRESS_ROOT"
 gosh-cli -j callx --abi ../contracts/l2/checker.abi.json --keys keys.json --addr $ADDRESS -m setRootContract --root $ADDRESS_ROOT
 PROP_CODE_WALLET=$(gosh-cli -j decode stateinit --tvc ../contracts/l2/TONTokenWallet.tvc | jq .code | cut -d '"' -f 2)
 PUBKEY=$(cat keys.json | jq  -r .public)
-gosh-cli -j deployx --abi ../contracts/l2/RootTokenContract.abi --keys keys.json ../contracts/l2/RootTokenContract.tvc --name "geth" --symbol "gth" --decimals 18 --root_pubkey $PUBKEY --root_owner null --total_supply 0 --checker $ADDRESS
+gosh-cli -j deployx --abi ../contracts/l2/RootTokenContract.abi --keys keys.json ../contracts/l2/RootTokenContract.tvc --name "geth" --symbol "gth" --decimals 18 --root_pubkey "0x$PUBKEY" --root_owner null --total_supply 0 --checker $ADDRESS
 gosh-cli -j callx --abi ../contracts/l2/RootTokenContract.abi --keys keys.json --addr $ADDRESS_ROOT -m setWalletCode --code $PROP_CODE_WALLET
 
