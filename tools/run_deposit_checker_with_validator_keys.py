6ae9fb64fd09691b361e0b889f2d77df7ee3af29
Alexander <alexander.s@gosh.sh>
2023-09-28 17:03:17 +0300

    key file to the temp path

--- tools/run_deposit_checker_with_validator_keys.py
+++ tools/run_deposit_checker_with_validator_keys.py
@@ -10,12 +10,11 @@ import base64
 # path to the validators config
 CONFIG_PATH = os.environ.get('VALIDATOR_CONFIG_PATH', '/opt/ton/ton-node/configs/config.json')
 GOSH_CLI = os.environ.get('GOSH_CLI_PATH', 'gosh-cli')
-VALIDATORS_KEY_PATH = os.environ.get('VALIDATORS_KEY_PATH', 'keys.json')
+TMP_VALIDATORS_KEY_PATH = os.environ.get('TMP_VALIDATORS_KEY_PATH', 'tmp/keys.json')
 DEPOSIT_PROPOSAL_CHECKER = os.environ.get('DEPOSIT_PROPOSAL_CHECKER', 'deposit-proposal-checker')
 
 
 def execute_cmd(cmd: str):
-    print(cmd)
     try:
         output = subprocess.check_output(cmd, shell=True).decode("utf-8")
     except subprocess.CalledProcessError as e:
@@ -34,8 +33,8 @@ def main():
     for key_id in mapping["validator_key_ring"]:
         key_ring = mapping["validator_key_ring"][key_id]
         private_key = base64.b64decode(key_ring["pvt_key"]).hex()
-        execute_cmd(f"{GOSH_CLI} getkeypair -p {private_key} -o {VALIDATORS_KEY_PATH}")
-        execute_cmd(f"VALIDATORS_KEY_PATH={VALIDATORS_KEY_PATH} {DEPOSIT_PROPOSAL_CHECKER}")
+        execute_cmd(f"{GOSH_CLI} getkeypair -p {private_key} -o {TMP_VALIDATORS_KEY_PATH}")
+        execute_cmd(f"VALIDATORS_KEY_PATH={TMP_VALIDATORS_KEY_PATH} {DEPOSIT_PROPOSAL_CHECKER}")
 
 main()
 
